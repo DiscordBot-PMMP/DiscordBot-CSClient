312e7ea285d7d8963928eb80602983eee631d24e
Jack Honour <JaxkDev@gmail.com>
2023-09-04 22:25:36 +0100

    Socket tests

--- DiscordBot-CSClient/Program.cs
+++ DiscordBot-CSClient/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// DiscordBot-CSClient
+//
+// Licensed under the Open Software License version 3.0 (OSL-3.0)
+// Copyright (C) 2023-present JaxkDev
+//
+// Discord :: JaxkDev
+// Email   :: JaxkDev@gmail.com
 
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("Starting socket.");
+
+IPAddress ipAddress = IPAddress.Parse("0.0.0.0");
+IPEndPoint ipEndPoint = new(ipAddress, 22_222);
+
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+
+Console.WriteLine("Listening for new connections...");
+
+var handler = await listener.AcceptAsync();
+
+Console.WriteLine("Client connected, waiting for initial packet...");
+
+while(true) {
+    // Receive message.
+    var buffer = new byte[1_024];
+    var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    Console.WriteLine(response);
+}
---
+++ DiscordBot-CSClient/SocketData.cs
@@ -0,0 +1,59 @@
+﻿// DiscordBot-CSClient
+//
+// Licensed under the Open Software License version 3.0 (OSL-3.0)
+// Copyright (C) 2023-present JaxkDev
+//
+// Discord :: JaxkDev
+// Email   :: JaxkDev@gmail.com
+
+using System.Net;
+
+namespace DiscordBot_CSClient;
+
+public class SocketData{
+
+	private readonly IPAddress ipAddress;
+	private readonly ushort port;
+
+	private List<List<byte>> outbound;
+    private List<List<byte>> inbound;
+
+    public SocketData(string ipAddress = "0.0.0.0", ushort port = 22222){
+		this.ipAddress = IPAddress.Parse(ipAddress);
+		this.port = port;
+	}
+
+	public IPAddress getIpAddress(){
+		return ipAddress;
+	}
+
+	public ushort getPort(){
+		return port;
+	}
+	
+	public List<byte>? readInbound(){
+		if(inbound.Count == 0) {
+			return null;
+		}
+		var c = inbound.First();
+		inbound.RemoveAt(0);
+		return c;
+	}
+
+	public List<byte>? readOutbound() {
+		if(outbound.Count == 0) {
+			return null;
+		}
+		var c = outbound.First();
+		outbound.RemoveAt(0);
+		return c;
+	}
+
+	public void writeInbound(List<byte> data){
+		inbound.Add(data);
+	}
+
+	public void writeOutbound(List<byte> data){
+		outbound.Add(data);
+	}
+}
